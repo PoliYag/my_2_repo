13e903d39b24116f86a7c051f38fd66ea613e037
PoliYag <150130450+PoliYag@users.noreply.github.com>
2023-11-30 19:57:23 +0300

    task_2
    Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Создаем массив на 10 целых чисел
+        int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        int evenCount = 0; // переменная для хранения количества четных чисел
+        Console.Write("Массив ["); 
+        // Проходим по каждому элементу массива
+        foreach (int number in numbers)
+        {
+             Console.Write(number + " ");
+            // Проверяем, является ли число четным
+            if (number % 2 == 0)
+            {
+                evenCount++; // увеличиваем счетчик на 1
+            }
+        }
+
+        // Выводим результат
+        Console.Write("] => " + evenCount); 
+
+        // Задержка консоли
+        Console.ReadLine();
+    }
+}
